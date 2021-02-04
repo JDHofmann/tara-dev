adaa91f023b5a655a2f65de1359b16910306a211
jdhofmann <justindhofmann@gmail.com>
2021-02-04 09:47:25 -0500

    footer added, layout added

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,15 @@
 import Header from "../components/header";
-
+import Footer from "../components/footer";
 
 export default function Layout({children})
  {
     return (
         <>
-        <Header />
-        <div>
-            {children}
-        </div>
+            <Header />
+            <div>
+                {children}
+            </div>
+            <Footer />
         </>
     )
 }
