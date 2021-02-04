c9dde5c326ca47fa79b7288170aeb6ccbd4f796a
jdhofmann <justindhofmann@gmail.com>
2021-02-04 09:52:35 -0500

    styling for footer

--- components/footer.js
+++ components/footer.js
@@ -2,6 +2,19 @@ export default function Footer(){
     return(
         <footer>
             <p>Made with love by JDBH</p>
+            <style jsx>{`
+                footer {
+                    height: 8vmax;
+                    background: #ff40ff;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                }
+                p {
+                    background: inherit;
+                    color: #101010;
+                }
+            `}</style>
         </footer>
     )
 }
