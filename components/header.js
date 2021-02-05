a37ad524aed0314bd7edbf5a7d87bdca4617c930
jdhofmann <justindhofmann@gmail.com>
2021-02-04 20:12:34 -0500

    menu button style

--- components/header.js
+++ components/header.js
@@ -10,6 +10,53 @@ export default function Header(){
                 <span></span>
             </button>
             <Menu />
+            <style jsx>{`
+                .burger-btn {
+                    height: 4vmax;
+                    max-height: 35px;
+                    margin: 2vmax;
+                    padding: 0;
+                    background: none;
+                    border: none;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
+                }
+                
+                .burger-btn-active {
+                    display: block;
+                }
+                
+                .burger-btn span {
+                    display: block;
+                    width: 4vmax;
+                    max-width: 35px;
+                    height: .65vmax;
+                    max-height: 5px;
+                    background: #ff40ff;
+                    border-radius: 2px;
+                    transition: .65s;
+                
+                }
+                
+                .burger-btn span:nth-child(2) {
+                    width: 3vmax;
+                    max-width: 27px;
+                }
+                
+                .burger-btn-active span:nth-child(1){
+                    transform: rotate(0.125turn);
+                }
+                
+                .burger-btn-active span:nth-child(2) {
+                    display: none;
+                }
+                
+                .burger-btn-active span:nth-child(3){
+                    transform: rotate(-0.125turn);
+                    margin-top: -.65vmax;
+                }
+            `}</style>
         </div>
     )
 }
